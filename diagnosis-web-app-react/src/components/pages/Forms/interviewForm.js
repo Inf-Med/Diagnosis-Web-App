eb55f066f818688424dba32268ebdabbf87efda9
mxrtia <41866138+mxrtia@users.noreply.github.com>
2020-08-04 21:13:53 +0200

    changes in submit button and interview form

--- diagnosis-web-app-react/src/components/pages/Forms/interviewForm.js
+++ diagnosis-web-app-react/src/components/pages/Forms/interviewForm.js
@@ -7,78 +7,78 @@ import { buildCorrectOptionsForSelectorFromApi } from '../../projectUtilities';
 
 class InterviewForm extends React.Component {
 
-        constructor() {
-          super();
-          this.state = {
-            "pregnancy": "false",
-            "cigarettes": "false",
-            "alcohol": "false",
-            "drugs": "",
-            "injury": "",
-            "family_diseases": "",
-            "count": 0,
+  constructor() {
+    super();
+    this.state = {
+      "pregnancy": "false",
+      "cigarettes": "false",
+      "alcohol": "false",
+      "drugs": "",
+      "injury": "",
+      "family_diseases": "",
+      "count": 0,
 
-            "selectorOptions": [],
-            "selectedSymptoms": [],
-          };
+      "selectorOptions": [],
+      "selectedSymptoms": [],
+    };
 
-          this.handleChanged = this.handleChanged.bind(this);
-          this.onRadioChange = this.onRadioChange.bind(this);
-          this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChanged = this.handleChanged.bind(this);
+    this.onRadioChange = this.onRadioChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
 
-        }
+  }
 
-          componentDidMount = () => {
-            this.fetchSymptomOptionsFromApi();
-          }
+  componentDidMount = () => {
+    this.fetchSymptomOptionsFromApi();
+  }
 
-          fetchSymptomOptionsFromApi = () => {
-            fetch('http://127.0.0.1:8000/quest/symptoms/')
-                .then(response => response.json())
-                .then(data => this.setState({selectorOptions: buildCorrectOptionsForSelectorFromApi(data)}));
-          }
+  fetchSymptomOptionsFromApi = () => {
+    fetch('http://127.0.0.1:8000/quest/symptoms/')
+      .then(response => response.json())
+      .then(data => this.setState({ selectorOptions: buildCorrectOptionsForSelectorFromApi(data) }));
+  }
 
-          handleSelectorChange = (selectedOptions) => {
-            this.setState({
-              selectedSymptoms: selectedOptions
-            })
-          }
+  handleSelectorChange = (selectedOptions) => {
+    this.setState({
+      selectedSymptoms: selectedOptions
+    })
+  }
 
-          handleChanged = (event) => {
-              this.setState({ [event.target.name] : event.target.value });
-          }
+  handleChanged = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  }
 
-          onRadioChange = (e) => {
-            this.setState({
-                [e.target.name]: e.target.value
-            });
-          }
+  onRadioChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
 
-          handleSubmit = (e) => {
-              e.preventDefault();
-              this.props.getMostFittingDiseasesAndSaveThemInAppJsState(this.state.selectedSymptoms);
-              this.props.sendInterview(this.state);
-              this.setState({
-                "pregnancy": "false",
-                "cigarettes": "false",
-                "alcohol": "false",
-                "drugs": "",
-                "injury": "",
-                "family_diseases": "",
-                "count": 1,
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.getMostFittingDiseasesAndSaveThemInAppJsState(this.state.selectedSymptoms);
+    this.props.sendInterview(this.state);
+    this.setState({
+      "pregnancy": "false",
+      "cigarettes": "false",
+      "alcohol": "false",
+      "drugs": "",
+      "injury": "",
+      "family_diseases": "",
+      "count": 1,
 
-                "selectorOptions": [],
-                "selectedSymptoms": [],
-                })
-              }
+      "selectorOptions": [],
+      "selectedSymptoms": [],
+    })
+  }
 
-          render() {
-              return (
-                  <form id="interview" className="form-style-5">
-                    {this.state.count !==1 &&
-                    <div>
-                       <h2>Are you pregnant:</h2>
-                       <br/>
+  render() {
+    return (
+      <form id="interview" className="form-style-5">
+        {this.state.count !== 1 &&
+          <div>
+            <h2>Are you pregnant:</h2>
+            <br />
             <ul>
               <li>
                 <label>
@@ -105,10 +105,10 @@ class InterviewForm extends React.Component {
                 </label>
               </li>
             </ul>
-                <br/>
+            <br />
 
-                <h2>Do you smoke?</h2>
-                <br/>
+            <h2>Do you smoke?</h2>
+            <br />
             <ul>
               <li>
                 <label>
@@ -135,86 +135,93 @@ class InterviewForm extends React.Component {
                 </label>
               </li>
             </ul>
-            <br/>
+            <br />
 
             <h2>Do you drink alcohol quite often?</h2>
-            <br/>
+            <br />
             <ul>
-            <li>
-            <label>
-            <input
-            name="alcohol"
-            type="radio"
-            value="false"
-            checked={this.state.alcohol === "false"}
-            onChange={this.onRadioChange}
-            />
-            <span>No</span>
-            </label>
-            </li>
-            <li>
-            <label>
-            <input
-            name="alcohol"
-            type="radio"
-            value="true"
-            checked={this.state.alcohol === "true"}
-            onChange={this.onRadioChange}
-            />
-            <span>Yes</span>
-            </label>
-            </li>
+              <li>
+                <label>
+                  <input
+                    name="alcohol"
+                    type="radio"
+                    value="false"
+                    checked={this.state.alcohol === "false"}
+                    onChange={this.onRadioChange}
+                  />
+                  <span>No</span>
+                </label>
+              </li>
+              <li>
+                <label>
+                  <input
+                    name="alcohol"
+                    type="radio"
+                    value="true"
+                    checked={this.state.alcohol === "true"}
+                    onChange={this.onRadioChange}
+                  />
+                  <span>Yes</span>
+                </label>
+              </li>
             </ul>
-                <br/>
+            <br />
 
-                <label>
-                <h2>Do you take any medicine or drugs?</h2>
-                    <input type="text" name="drugs"
-                        value={ this.state.drugs }
-                        onChange={ this.handleChanged }
-                        />
-                </label>
+            <label>
+              <h2>Do you take any medicine or drugs?</h2>
+              <input type="text" name="drugs"
+                value={this.state.drugs}
+                onChange={this.handleChanged}
+              />
+            </label>
 
-                <label>
-                <h2>Did you have any injuries recently?</h2>
-                    <input type="text" name="injury"
-                        value={ this.state.injury }
-                        onChange={ this.handleChanged }
-                        />
-                </label>
-                <label>
-                <h2>What are your symptoms?</h2>
+            <label>
+              <h2>Did you have any injuries recently?</h2>
+              <input type="text" name="injury"
+                value={this.state.injury}
+                onChange={this.handleChanged}
+              />
+            </label>
 
-                <Select options={ this.state.selectorOptions } isMulti={ true } onChange={ this.handleSelectorChange } />
+            <label>
+              <h2>Are there any diseases in your family?</h2>
+              <input type="text" name="family_diseases"
+                value={this.state.family_diseases}
+                onChange={this.handleChanged}
+              />
+            </label>
 
-                </label>
+            <label>
+              <h2>What are your symptoms?</h2>
+              <Select options={this.state.selectorOptions} isMulti={true} onChange={this.handleSelectorChange} />
+            </label>
 
-                <label>
-                <h2>Are there any diseases in your family?</h2>
-                    <input type="text" name="family_diseases"
-                        value={ this.state.family_diseases }
-                        onChange={ this.handleChanged }
-                        />
-                </label>
-                <br/>
-                </div>
-              }
-                {this.state.family_diseases !=='' && this.state.injury !=='' && this.state.drugs !==''
-                && this.state.selectedSymptoms !== null && this.state.selectorOptions !== null &&
-                <button onClick={ this.handleSubmit }>Submit</button>
-              }
-              {this.state.count ===1 &&
-              <div>
-              <Link to="/diagnosis">
-                  <button >
-                      Next Page
-                  </button>
-              </Link>
-              </div>
-              }
-                </form>
-        )
-    }
+            <br />
+          </div>
+        }
+
+
+        {this.state.family_diseases !== '' && this.state.injury !== '' && this.state.drugs !== ''
+          && this.state.selectedSymptoms.length > 0 &&
+
+          <button onClick={this.handleSubmit}>
+            <Link to="/diagnosis">
+              Submit
+            </Link>
+          </button>
+        }
+
+        {this.state.count === 1 &&
+          <div>
+            <Link to="/diagnosis">
+              <button>Next Page</button>
+            </Link>
+          </div>
+        }
+
+      </form>
+    )
+  }
 }
 
 
--- diagnosis-web-app-react/src/components/pages/Forms/questForm.js
+++ diagnosis-web-app-react/src/components/pages/Forms/questForm.js
@@ -7,21 +7,21 @@ class QuestForm extends React.Component {
     super();
     this.state = {
       "first_name": "",
-      "first_nameError":"",
-      "first_nameError2":"",
+      "first_nameError": "",
+      "first_nameError2": "",
       "last_name": "",
-      "last_nameError2":"",
-      "last_nameError":"",
+      "last_nameError2": "",
+      "last_nameError": "",
       "age": 0,
       "date_of_birth": "",
-      "dateError":"",
-      "dateError2":"",
-      "dateError3":"",
+      "dateError": "",
+      "dateError2": "",
+      "dateError3": "",
       "pesel": "",
-      "peselError":"",
-      "peselError2":"",
+      "peselError": "",
+      "peselError2": "",
       "sex": 'f',
-      "count":0,
+      "count": 0,
     };
 
     this.onRadioChange = this.onRadioChange.bind(this);
@@ -29,200 +29,208 @@ class QuestForm extends React.Component {
 
   }
 
-    LastChanged = (event) => {
-      this.setState({ last_name : event.target.value }, () =>{
-        this.validatelast_name();
-      });
+  LastChanged = (event) => {
+    this.setState({ last_name: event.target.value }, () => {
+      this.validatelast_name();
+    });
   };
-    validatelast_name = (e) =>{
-      const{last_name} = this.state;
-      this.setState({
-        last_nameError:
+  validatelast_name = (e) => {
+    const { last_name } = this.state;
+    this.setState({
+      last_nameError:
         last_name.length > 2 || last_name === '' ? null : 'Last name must be longer than 2 characters',
-        last_nameError2:
-        isNaN(last_name) ? null : 'Last name cannot contain number'
-      });
-    }
-    FirstChanged = (event) => {
-      this.setState({ first_name : event.target.value }, () =>{
-        this.validatefirst_name();
-      });
+      last_nameError2:
+        isNaN(last_name) ? null : 'Last name cannot contain number/cannot be empty'
+    });
+  }
+  FirstChanged = (event) => {
+    this.setState({ first_name: event.target.value }, () => {
+      this.validatefirst_name();
+    });
   };
-    validatefirst_name = (e) =>{
-      const{first_name} = this.state;
-      this.setState({
-        first_nameError:
+  validatefirst_name = (e) => {
+    const { first_name } = this.state;
+    this.setState({
+      first_nameError:
         first_name.length > 2 || first_name === '' ? null : 'First name must be longer than 2 characters',
-        first_nameError2:
-        isNaN(first_name) ? null : 'First name cannot contain number'
-      });
-    }
+      first_nameError2:
+        isNaN(first_name) ? null : 'First name cannot contain number/cannot be empty'
+    });
+  }
 
-    PeselChanged = (event) => {
-      this.setState({ pesel : event.target.value }, () =>{
-        this.validatepesel();
-      });
+  PeselChanged = (event) => {
+    this.setState({ pesel: event.target.value }, () => {
+      this.validatepesel();
+    });
   };
-    validatepesel = (e) =>{
-      const{pesel} = this.state;
-      var reg = new RegExp('^[0-9]{11}$');
-      var date_of_birth = (this.state.date_of_birth).toString().substring(0,4)+(this.state.date_of_birth).toString().substring(5,7)+(this.state.date_of_birth).toString().substring(8,10);
-      var date_of_birth_00 = (this.state.date_of_birth).toString().substring(2,4)+"2"+(this.state.date_of_birth).toString().substring(6,7)+(this.state.date_of_birth).toString().substring(8,10);
-      var pesel_S = (pesel).toString()
-      this.setState({
-        peselError:
+  validatepesel = (e) => {
+    const { pesel } = this.state;
+    var reg = new RegExp('^[0-9]{11}$');
+    var date_of_birth = (this.state.date_of_birth).toString().substring(0, 4) + (this.state.date_of_birth).toString().substring(5, 7) + (this.state.date_of_birth).toString().substring(8, 10);
+    var date_of_birth_00 = (this.state.date_of_birth).toString().substring(2, 4) + "2" + (this.state.date_of_birth).toString().substring(6, 7) + (this.state.date_of_birth).toString().substring(8, 10);
+    var pesel_S = (pesel).toString()
+    this.setState({
+      peselError:
         reg.test(pesel) ? null : 'Pesel is in the wrong format',
-        peselError2:
-        date_of_birth.startsWith("20")?
-        pesel_S.startsWith(date_of_birth_00) ? null : 'Pesel does not match the date of birth':
-        pesel_S.startsWith(date_of_birth.substring(2,7)) ? null : 'Pesel does not match the date of birth',
-      });
-    }
-    DateChanged = (event) => {
-      var age_count = new Date(event.target.value).getFullYear() === new Date().getFullYear()? 1: parseInt(moment(new Date(event.target.value),"yyyy-MM-dd").fromNow())-1;
-      this.setState({ date_of_birth : event.target.value,
-      age: age_count}, () =>{
-        this.validatedate();
-      });
+      peselError2:
+        date_of_birth.startsWith("20") ?
+          pesel_S.startsWith(date_of_birth_00) ? null : 'Pesel does not match the date of birth' :
+          pesel_S.startsWith(date_of_birth.substring(2, 7)) ? null : 'Pesel does not match the date of birth',
+    });
+  }
+  DateChanged = (event) => {
+    var age_count = new Date(event.target.value).getFullYear() === new Date().getFullYear() ? 1 : parseInt(moment(new Date(event.target.value), "yyyy-MM-dd").fromNow()) - 1;
+    this.setState({
+      date_of_birth: event.target.value,
+      age: age_count
+    }, () => {
+      this.validatedate();
+    });
   };
-    validatedate = (e) =>{
-      const{date_of_birth} = this.state;
-      var reg = new RegExp('^(19|20)[0-9]{2}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$');
-      const date_now = new Date()
-      this.setState({
-        dateError:
-        reg.test(date_of_birth)? null : 'Date is in the wrong format',
-        dateError2:
-        new Date(date_of_birth) < date_now? null : 'Date is from future',
-        dateError3:
-        new Date(date_of_birth).getFullYear() > parseInt(new Date().getFullYear())-99 ? null : 'Have you more than 99 years ?',
-      });
-    }
-    onRadioChange = (e) => {
-      this.setState({
-        sex: e.target.value
-      });
-    }
+  validatedate = (e) => {
+    const { date_of_birth } = this.state;
+    var reg = new RegExp('^(19|20)[0-9]{2}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])$');
+    const date_now = new Date()
+    this.setState({
+      dateError:
+        reg.test(date_of_birth) ? null : 'Date is in the wrong format',
+      dateError2:
+        new Date(date_of_birth) < date_now ? null : 'Date is from future',
+      dateError3:
+        new Date(date_of_birth).getFullYear() > parseInt(new Date().getFullYear()) - 99 ? null : 'Are you older than 99?',
+    });
+  }
+  onRadioChange = (e) => {
+    this.setState({
+      sex: e.target.value
+    });
+  }
 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        this.setState({
-          "count": 1,
-        })
-        this.props.sendQuestRequest(this.state);
-        this.setState({
-          "first_name": "",
-          "last_name": "",
-          "age": 0,
-          "date_of_birth": "",
-          "pesel": "",
-          "sex": "f"
-      })
-    }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({
+      "count": 1,
+    })
+    this.props.sendQuestRequest(this.state);
+    this.setState({
+      "first_name": "",
+      "last_name": "",
+      "age": 0,
+      "date_of_birth": "",
+      "pesel": "",
+      "sex": "f"
+    })
+  }
 
-    render() {
-        return (
-            <form id="quest" className="form-style-5">
-            {this.state.count !==1 &&
+  render() {
+    return (
+      <form id="quest" className="form-style-5">
+        {this.state.count !== 1 &&
+          <div>
             <div>
-              <div>
-                <label>
-                    <h3>First name:</h3>
-                    <input type="text" name="first_name"
-                        value={ this.state.first_name }
-                        onChange={ this.FirstChanged }
-                        className={`form-control ${this.state.first_nameError ? 'is-invalid' : ''}`}
-                        onBlur={this.validatefirst_name}
-                        />
-
-                </label>
-                <div className='invalid-feedback'>{this.state.first_nameError}</div>
-                <div className='invalid-feedback'>{this.state.first_nameError2}</div>
-        </div>
-        <br/>
-        <div>
-                <label>
-                    <h3>Last name:</h3>
-                    <input type="text" name="last_name"
-                        value={ this.state.last_name }
-                        onChange={ this.LastChanged}
-                        onBlur={this.validatelast_name}
-                        />
-
-                </label>
-                <div className='invalid-feedback'>{this.state.last_nameError}</div>
-                <div className='invalid-feedback'>{this.state.last_nameError2}</div>
-        </div>
-        <br/>
-         <div>
-                <h3>Sex:</h3>
-                <br/>
-          <ul>
-            <li>
               <label>
-                <input
-                  type="radio"
-                  value="f"
-                  checked={this.state.sex === "f"}
-                  onChange={this.onRadioChange}
+                <h3>First name:</h3>
+                <input type="text" name="first_name"
+                  value={this.state.first_name}
+                  onChange={this.FirstChanged}
+                  className={`form-control ${this.state.first_nameError ? 'is-invalid' : ''}`}
+                  onBlur={this.validatefirst_name}
                 />
-                <span>Female</span>
+
               </label>
-            </li>
-            <li>
+              <div className='invalid-feedback'>{this.state.first_nameError}</div>
+              <div className='invalid-feedback'>{this.state.first_nameError2}</div>
+            </div>
+            <br />
+            <div>
               <label>
-                <input
-                  type="radio"
-                  value="m"
-                  checked={this.state.sex === "m"}
-                  onChange={this.onRadioChange}
+                <h3>Last name:</h3>
+                <input type="text" name="last_name"
+                  value={this.state.last_name}
+                  onChange={this.LastChanged}
+                  onBlur={this.validatelast_name}
                 />
-                <span>Male</span>
+
               </label>
-            </li>
-          </ul>
-          </div>
-                <br/>
-<div>
-                <label>
-                    <h3>Date of birth:</h3>
+              <div className='invalid-feedback'>{this.state.last_nameError}</div>
+              <div className='invalid-feedback'>{this.state.last_nameError2}</div>
+            </div>
+            <br />
+            <div>
+              <h3>Sex:</h3>
+              <br />
+              <ul>
+                <li>
+                  <label>
                     <input
-                        type="text"
-                        name="date_of_birth"
-                        value={ this.state.date_of_birth }
-                        onChange={ this.DateChanged }
-                        placeholder={"YYYY-MM-DD"}
-                        onBlur={this.validatedate}
-                        />
-                </label>
-                <br/>
-                <div className='invalid-feedback'>{this.state.dateError}</div>
-                <div className='invalid-feedback'>{this.state.dateError2}</div>
-                <div className='invalid-feedback'>{this.state.dateError3}</div>
-        </div>
+                      type="radio"
+                      value="f"
+                      checked={this.state.sex === "f"}
+                      onChange={this.onRadioChange}
+                    />
+                    <span>Female</span>
+                  </label>
+                </li>
+                <li>
+                  <label>
+                    <input
+                      type="radio"
+                      value="m"
+                      checked={this.state.sex === "m"}
+                      onChange={this.onRadioChange}
+                    />
+                    <span>Male</span>
+                  </label>
+                </li>
+              </ul>
+            </div>
+            <br />
+            <div>
+              <label>
+                <h3>Date of birth:</h3>
+                <input
+                  type="text"
+                  name="date_of_birth"
+                  value={this.state.date_of_birth}
+                  onChange={this.DateChanged}
+                  placeholder={"YYYY-MM-DD"}
+                  onBlur={this.validatedate}
+                />
+              </label>
+              <br />
+              <div className='invalid-feedback'>{this.state.dateError}</div>
+              <div className='invalid-feedback'>{this.state.dateError2}</div>
+              <div className='invalid-feedback'>{this.state.dateError3}</div>
+            </div>
 
-        <div>
-                <label>
-                    <h3>PESEL:</h3>
-                    <input type="text" name="pesel"
-                        value={ this.state.pesel }
-                        onChange={ this.PeselChanged }
-                        onBlur={this.validatepesel}
-                        />
+            <div>
+              <label>
+                <h3>PESEL:</h3>
+                <input type="text" name="pesel"
+                  value={this.state.pesel}
+                  onChange={this.PeselChanged}
+                  onBlur={this.validatepesel}
+                />
 
-                </label>
-                <br/>
-                <div className='invalid-feedback'>{this.state.peselError}</div>
-                <div className='invalid-feedback'>{this.state.peselError2}</div>
-                <br/>
-        </div>
-        </div>
-    }
-                {this.state.pesel !=='' &&  this.state.date_of_birth !=='' && this.state.first_name !=='' && this.state.last_name !=='' && !this.state.peselError && !this.state.peselError2 && !this.state.ageError && !this.state.last_nameError && !this.state.last_nameError2 && !this.state.fiest_nameError2 && !this.state.first_nameError && !this.state.dateError && !this.state.dateError2 && !this.state.dateError3 &&
-                <button type = "submit" onClick={this.handleSubmit}>Submit
-                </button>
-              }
-              {this.state.count ===1 &&
+              </label>
+              <br />
+              <div className='invalid-feedback'>{this.state.peselError}</div>
+              <div className='invalid-feedback'>{this.state.peselError2}</div>
+              <br />
+            </div>
+          </div>
+        }
+        {this.state.pesel !== '' && this.state.date_of_birth !== '' && this.state.first_name !== '' &&
+          this.state.last_name !== '' && !this.state.peselError && !this.state.peselError2 && !this.state.ageError
+          && !this.state.last_nameError && !this.state.last_nameError2 && !this.state.fiest_nameError2 &&
+          !this.state.first_nameError && !this.state.dateError && !this.state.dateError2 && !this.state.dateError3 &&
+          <button onClick={this.handleSubmit}>
+            <Link to="/interview">
+              Submit
+            </Link>
+          </button>
+        }
+        {this.state.count ===1 &&
               <div>
               <Link to="/interview">
                   <button >
@@ -231,10 +239,10 @@ class QuestForm extends React.Component {
               </Link>
               </div>
               }
-            </form>
+      </form>
 
-        )
-    }
+    )
   }
+}
 
 export default QuestForm;
